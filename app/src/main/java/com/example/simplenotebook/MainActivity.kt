d6fa3bd4d7f38de19513b25bbb6688a669141148
rafi <rafiwilder4204@gmail.com>
2024-05-12 02:53:06 +0600

    refactor: ai reviewr test

--- app/src/main/java/com/example/simplenotebook/MainActivity.kt
+++ app/src/main/java/com/example/simplenotebook/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleNotebookTheme {
+             SimpleNotebookTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
